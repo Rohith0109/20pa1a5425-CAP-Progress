5140e25c56042e82e11a3383cfae87476b7388de
Rohith0109 <116626569+Rohith0109@users.noreply.github.com>
2023-01-27 00:28:45 +0530

    Changed the code in flattenBinaryTree.java

--- 26thJan2023/FlattenBinaryTree.java
+++ 26thJan2023/FlattenBinaryTree.java
@@ -1,14 +1,35 @@
 class FlatternBinaryTree {
+   
     TreeNode curr = null;
-    public void flatten(TreeNode root) {
-        if (root==null)
+    public void intoTree(ArrayList<TreeNode> al){
+        if (al.size()==0)
         return;
-        
-        flatten(root.right);
-        flatten(root.left);
-        root.right = curr;
-        root.left = null;
-        curr = root;
-        
+
+        TreeNode dummy = al.get(0);
+        curr = dummy;
+        TreeNode temp = curr;
+        for (int i=1;i<al.size();i++){
+            dummy = al.get(i);
+            temp.right = dummy;
+            temp.left = null;
+            temp = dummy;
+        }
+
+    }
+
+    ArrayList<TreeNode> ls = new ArrayList<>();
+    public ArrayList<TreeNode> preorder (TreeNode root){
+        if (root!=null){
+            ls.add(root);
+            preorder(root.left);
+            preorder(root.right);
+        }
+        return ls;
+    }
+
+    public void flatten(TreeNode root) {
+        preorder(root);
+        intoTree(ls);
+        root = curr;
     }
 }
